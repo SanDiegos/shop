19d1b6edc8c5f87086d6b1dd194c216f6a6a311b
die-g <die-g@192.168.0.3>
2020-01-30 13:31:14 +0100

    JDBC - antMatchers. storage - authenticated

--- shop/src/main/java/com/djedra/shop/configuration/security/SecurityConfig.java
+++ shop/src/main/java/com/djedra/shop/configuration/security/SecurityConfig.java
@@ -1,31 +1,37 @@
 package com.djedra.shop.configuration.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 @Configuration
 @EnableResourceServer
 public class SecurityConfig extends ResourceServerConfigurerAdapter {
 
-//	CRM_REALM takie same co w konfiguracji serwisu autoryzacyjnego
-	@Override
-	@Order(Ordered.HIGHEST_PRECEDENCE)
-	public void configure(HttpSecurity http) throws Exception {
-		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-		.and()
-//		.csrf().disable()
-		.authorizeRequests().anyRequest().authenticated()
-		.and().httpBasic().realmName("CRM_REALM");
-	}
+	@Autowired
+    private ResourceServerTokenServices tokenServices;
 
-//	@Override
-//	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-//		// do not require a resource id in AccessToken.
-//		resources.resourceId(null);
-//	}
+    @Autowired
+    private JwtAccessTokenConverter accessTokenConverter;
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenServices(tokenServices);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+            .requestMatchers()
+            .and()
+            .authorizeRequests()
+            .antMatchers("/article/**", "/articleCategory/**","/oauth/*").permitAll()
+            .antMatchers("/storage/**" ).authenticated();
+    }
+
 }
